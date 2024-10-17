f3617019cdf1dbc749481e3a93d4575e526e31bd
Guizmohackett <46965883+gdelacroix@users.noreply.github.com>
2024-10-17 12:24:10 +0200

    mise à jour des infos de déploiement

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>  <App /></BrowserRouter>
+    <BrowserRouter basename="/Infos_Films_With_Vite">  <App /></BrowserRouter>
   
   </React.StrictMode>
 );
